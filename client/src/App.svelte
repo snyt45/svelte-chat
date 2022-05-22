c438507ea8752d75e83058fc444b5df1ebd7e62d
snyt45 <snyt45@gmail.com>
2022-05-22 18:31:56 +0900

    クライアント側のモック作成

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,30 +1,68 @@
 <script lang="ts">
-	export let name: string;
+  let messages: { userId: string; message: string }[] = []
+  let testDate = [
+    { userId: "test0001", message: "test1"},
+    { userId: "test0002", message: "test2"},
+    { userId: "test0001", message: "test3"},
+    { userId: "test0002", message: "test4"},
+    { userId: "test0001", message: "test5"}
+  ]
+  messages = testDate
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <div class="wrapper">
+    <h1>Svelte Chat</h1>
+    <div class="board">
+      <div class="mine">
+        <p class="message">ここに文章</p>
+      </div>
+      <div>
+        <p class="message">ここに文章</p>
+      </div>
+      <div class="mine">
+        <p class="message">ここに文章</p>
+      </div>
+      <div>
+        <p class="message">ここに文章</p>
+      </div>
+    </div>
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    max-width: 400px;
+    margin: 0 auto;
+  }
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    text-align: center;
+    font-size: 2em;
+    font-weight: 400;
+  }
+  .board {
+    padding: 1em;
+    background-color: #ebeeee;
+    height: 80vh;
+    overflow: scroll;
+    scroll-behavior: smooth;
+  }
+  .mine {
+    display: flex;
+    flex-direction: row-reverse;
+  }
+  .message {
+    border-radius: 10px;
+    padding: 10px;
+    background-color: #fff;
+    display: inline-block;
+  }
+  .mine .message {
+    background-color: #85e249;
+  }
 </style>
